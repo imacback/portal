1dede14585961598fd4630eb9edc76c29cca911e
imacback <imacback@gmail.com>
2017-09-08 22:06:49 +0800

    Initial commit

---
+++ src/main/java/com/google/controller/LoginController.java
@@ -0,0 +1,19 @@
+package com.google.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+
+    @RequestMapping("/index")
+    public ModelAndView index(HttpServletRequest request, HttpServletResponse response) {
+        System.out.println("============");
+        return null;
+    }
+}
