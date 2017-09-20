b6b6a5299b4623d4b44efd11b06b02db1d7a7d42
imacback <imacback@gmail.com>
2017-09-20 12:48:10 +0800

    增加junit支持

---
+++ src/test/java/com/imac/sync/BaseClassTest.java
@@ -0,0 +1,38 @@
+package com.imac.sync; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* BaseClass Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>09/20/2017</pre> 
+* @version 1.0 
+*/ 
+public class BaseClassTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+
+/** 
+* 
+* Method: dosomething() 
+* 
+*/ 
+@Test
+public void testDosomething() throws Exception { 
+//TODO: Test goes here...
+	System.out.println("1111111111");
+} 
+
+
+
+} 
