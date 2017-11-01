325992fe7577acc78cb2da1497a2a4ecda9f1a13
imacback <imacback@gmail.com>
2017-11-01 14:02:18 +0800

    增加log4j日志配置

--- src/main/java/com/google/controller/LoginController.java
+++ src/main/java/com/google/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.google.controller;
 
 import com.google.pojo.Person;
 import com.imac.json.Json;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,9 +14,14 @@ import javax.servlet.http.HttpServletResponse;
 @RequestMapping("/login")
 public class LoginController {
 
+	private static final Logger LOGGER = Logger.getLogger(LoginController.class);
+
     @RequestMapping("/index")
 	@Json(type = Person.class, include = "id")
     public Person index(HttpServletRequest request, HttpServletResponse response) {
+
+		LOGGER.info("login");
+
         return new Person();
     }
 
